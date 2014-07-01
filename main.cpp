017c992b8c1549acff769221df0206fd8f28438c
Enzhaev Ivan <8observer8@gmail.com>
2014-07-01 22:45:09 +0400

    First Commit

--- main.cpp
+++ main.cpp
@@ -1,81 +1,42 @@
 
 #include <vector>
 #include <iostream>
-
 #include <QString>
-
 #include "freeFunctions.h"
 #include "Person.h"
-#include "LogicError.h"
-#include "FileError.h"
 
 int main( )
 {
     // Person array for saving
     Person david( "David", "White" );
     Person ivan( "Ivan", "Green" );
-    std::vector<Person> personsOut;
-    personsOut.push_back( david );
-    personsOut.push_back( ivan );
+    std::vector<Person> persons;
+    persons.push_back( david );
+    persons.push_back( ivan );
 
-    // Parse the person array to the XML-document
-    QDomDocument docOut;
     try {
-        parsePersonsToXml( personsOut, docOut );
-    } catch ( const LogicError &e ) {
-        std::cerr << e.what( ) << std::endl;
-        return 1;
-    } catch ( ... ) {
-        std::cerr << "Error: unknown exception" << std::endl;
-        return 1;
-    }
+        // Parse the person array to the XML-content
+        QDomDocument content;
+        parsePersonsToXml( persons, content );
 
-    // Write the XML-document to the file
-    QString fileName = "Persons.xml";
-    try {
-        writeData( fileName, docOut );
-    } catch ( const LogicError &e ) {
-        std::cerr << e.what( ) << std::endl;
-        return 1;
-    } catch ( const FileError &e ) {
-        std::cerr << e.what( ) << std::endl;
-        return 1;
-    } catch ( ... ) {
-        std::cerr << "Error: unknown exception" << std::endl;
-        return 1;
-    }
+        // Write the XML-content to the file
+        QString fileName = "Persons.xml";
+        writeData( fileName, content );
 
-    // Read the XML-document from the file
-    QDomDocument docIn;
-    try {
-        readData( fileName, docIn );
+        // Read the XML-content from the file
+        QDomDocument readContent;
+        readData( fileName, readContent );
+
+        // Parse the XML-content to the person array
+        std::vector<Person> readPersons;
+        parseXmlToPersons( readContent, readPersons );
+
+        // Print the person array on the screen
+        printData( readPersons );
     } catch ( const LogicError &e ) {
         std::cerr << e.what( ) << std::endl;
         return 1;
     } catch ( const FileError &e ) {
-        std::cerr << e.what( ) << std::endl;
-        return 1;
-    } catch ( ... ) {
-        std::cerr << "Error: unknown exception" << std::endl;
-        return 1;
-    }
-
-    // Parse the XML-document to the person array
-    std::vector<Person> personsIn;
-    try {
-        parseXmlToPersons( docIn, personsIn );
-    } catch ( const LogicError &e ) {
-        std::cerr << e.what( ) << std::endl;
-        return 1;
-    } catch ( ... ) {
-        std::cerr << "Error: unknown exception" << std::endl;
-        return 1;
-    }
-
-    // Print the person array on the screen
-    try {
-        printData( personsIn );
-    } catch ( const LogicError &e ) {
         std::cerr << e.what( ) << std::endl;
         return 1;
     } catch ( ... ) {
